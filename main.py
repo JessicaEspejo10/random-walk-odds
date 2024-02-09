5c4c5737f499c3ad3d560414f6d85030aea67baf
Jessica Espejo <jenespejoto@unal.edu.co>
2024-02-09 17:37:25 -0500

    distribution visualization and odds

--- main.py
+++ main.py
@@ -4,24 +4,43 @@ import matplotlib.pyplot as plt
 #setting the seed
 np.random.seed(123)
 
+#clear the plot
+plt.clf()
 
-# Initialization
-random_walk = [0]
+all_walks = []
+for i in range(500):
+    # Initialization
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        #roll the dice
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        # Implement clumsiness
+        if np.random.rand() <= 0.005 :
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
 
-for x in range(100) :
-    step = random_walk[-1]
-    dice = np.random.randint(1,7)
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
 
-    if dice <= 2:
-        step = max(0, step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1,7)
+# Select last row from np_aw_t: ends
+ends = np_aw_t[100,:]
 
-    random_walk.append(step)
-
-#plot random walk
-plt.plot(random_walk)
-
+# Plot histogram of ends, display plot
+plt.hist(ends)
 plt.show()
+
+odd = 0
+#calculate the odds
+for e in ends:
+    if e >= 60:
+        odd += 1
+odd = odd / 500
+print("the probability foy you to win is about " + str(odd * 100) + "%")
