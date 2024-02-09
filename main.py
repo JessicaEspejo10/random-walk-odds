a91497c344f83c01d05da6e40b85b38422c09535
Jessica Espejo <jenespejoto@unal.edu.co>
2024-02-09 16:45:17 -0500

    setting dice rolls

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-#generar números pseudoaleatorios para el lanzamiento del dado
-np.random.randint()
+#setting the seed
+np.random.seed(123)
+
+#starting step
+step = 0
+
+#roll the dice
+dice = np.random.randint(1,7)
+
+#control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <6 :
+    step =+ 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print("dice: " + str(dice) + " - Step: " + str(step))
